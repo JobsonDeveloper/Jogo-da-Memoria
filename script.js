ef2e6933f938fd77577a15a7a53066748735261e
Jobson Oliveira <jobsondeveloper@gmail.com>
2024-12-09 23:31:58 -0300

    Iniciando as verificações no Js

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 const cards = document.querySelectorAll('.card');
+let hasFlipperCard = false;
+let firstCard, secondCard;
 
 function flipCard() {
     // Adiciona a classe 'flip no card' para que o css vire a carta que foi selecionada
-    // O método 'toggle' retira a classe se ela existir e adiciona se ela não existir 
-    this.classList.toggle('card-flip');
+    this.classList.add('card-flip');
+ 
+    if (!hasFlipperCard) {
+        hasFlipperCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    hasFlipperCard = false;
+    ckeckForMath()
+}
+
+function ckeckForMath() {
+    if(firstCard.dataset.card === secondCard.dataset.card) {
+        disableCards();
+        return;
+    }
+    else {
+        unflipCards();
+    }
 }
 
 cards.forEach((card) => {
     // Para cada card clicado, será realizsada a função flipCard (Girar o card)
     card.addEventListener('click', flipCard)
-})
+});
